a3400febb10adc98c464ba047401a0346899786b
wendy6yu <wendy_yu@college.harvard.edu>
2020-08-05 00:46:45 -0400

    ui page styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,18 @@ $(document).ready(function(){
 	});
  
  });
+
+var coll = $(".collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
